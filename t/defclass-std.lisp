f2b26b6d944a40011d962d94ac2004fb522475bb
André <andremiramor@gmail.com>
2014-12-15 20:33:57 -0200

    Added test

--- t/defclass-std.lisp
+++ t/defclass-std.lisp
@@ -4,8 +4,25 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :defclass-std)' in your Lisp.
 
-(plan nil)
+(plan 1)
 
-
+(deftest test-all-keyword-option
+  (is-expand (defclass/std computer (gadget)
+               ((screen mouse keyboard :a :type string :with-prefix)
+                (bluetooth touchpad :wi)
+                (speaker microphone :r)
+                (place :@@ :with :doc "Where it is" :r)
+                (owner :static :std "Me" :w)))
+             (DEFCLASS COMPUTER (GADGET)
+               ((SCREEN :ACCESSOR COMPUTER-SCREEN :INITFORM NIL :TYPE STRING)
+                (MOUSE :ACCESSOR COMPUTER-MOUSE :INITFORM NIL :TYPE STRING)
+                (KEYBOARD :ACCESSOR COMPUTER-KEYBOARD :INITFORM NIL :TYPE STRING)
+                (BLUETOOTH :WRITER BLUETOOTH :INITARG :BLUETOOTH :INITFORM NIL)
+                (TOUCHPAD :WRITER TOUCHPAD :INITARG :TOUCHPAD :INITFORM NIL)
+                (SPEAKER :READER SPEAKER :INITFORM NIL)
+                (MICROPHONE :READER MICROPHONE :INITFORM NIL)
+                (PLACE :READER COMPUTER-PLACE :INITFORM NIL :ALLOCATION :CLASS
+                       :DOCUMENTATION "Where it is")
+                (OWNER :WRITER OWNER :INITFORM "Me" :ALLOCATION :CLASS)))))
 
 (run-test-all)
