691c7ff849d04ae16cc5175580b0f4f4f65bf021
André Miranda <andremiramor@gmail.com>
2015-03-31 10:25:58 -0300

    Refactoring FIND-FUSIONED-KEYWORD-OPTIONS

--- src/defclass-std.lisp
+++ src/defclass-std.lisp
@@ -45,8 +45,8 @@
                      ((> (position (first elements) list)
                          (position (second elements) list))
                       (first-el  (cdr elements) list))
-                     (t (let ((list-without-second-el (cons (first elements)
-                                                            (cddr elements)))))
+                     (t (let ((list-without-second-el
+                               (cons (first elements) (cddr elements)))))
                         (first-el list-without-second-el list))))))
     (let* ((maybe-unknown-keywords (set-difference (remove-if-not #'keywordp line)
                                                    *options*))
